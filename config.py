a1b1ba40d84f728a3c9e77cdd5c882ee66233095
Joseph Jameson (VCO - Staff) <zue12aeu@UEA.AC.UK>
2026-07-06 16:40:18 +0100

    updated new data

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 
 
 uea_current_scores = {
-    'Academic Reputation': 24.7,
-    'Citations per Faculty': 55.5,
-    'Employer Reputation' : 15.0,
-    'Faculty Student Ratio' : 24.0,
-    'Employment Outcomes' : 31.8,
-    'International Faculty Ratio' : 81.9,
-    'International Research Network' : 86.8,
-    'International Student Ratio' : 49.3,
-    'Sustainability' : 91.1,
-    'International Student Diversity' : 54.0
+    'Academic Reputation': 22.9,
+    'Citations per Faculty': 58.9,
+    'Employer Reputation' : 17.1,
+    'Faculty Student Ratio' : 24.7,
+    'Employment Outcomes' : 33.5,
+    'International Faculty Ratio' : 83.2,
+    'International Research Network' : 83.6,
+    'International Student Ratio' : 43.2,
+    'Sustainability' : 91.8,
+    'International Student Diversity' : 0.0
     }
